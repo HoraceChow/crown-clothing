ddd9a2ce495b29357763af1425eaa87af68a818c
Horace <horace1028@gmail.com>
2020-11-22 22:38:03 +0800

    included react router dom, and begin to develop the shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
-    <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+/*
+included scss, react-router-dom
+*/
