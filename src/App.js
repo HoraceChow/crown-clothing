f0f2c9a5420aed267e87a09ed0bbbfde65ece372
Horace <horace1028@gmail.com>
2020-11-23 23:23:08 +0800

    added the header for page, and included link to different pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from './component/header/header.component.jsx';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
@@ -18,4 +19,6 @@ function App() {
 export default App;
 /*
 included scss, react-router-dom
+
+
 */
