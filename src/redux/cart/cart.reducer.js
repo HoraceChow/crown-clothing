5fa9a706abb9cad29d24d4ea5d82228085cdf828
Horace <horace1028@gmail.com>
2020-11-29 02:28:21 +0800

    update shop data, added the functionality of add to cart and processing item quantities with redux

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,8 @@
-import CartActionTypes from './cart.types.js'
+import CartActionTypes from './cart.types.js';
+import {addItemToCart} from './cart.utils';
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItem: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,6 +11,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return{
                 ...state,
                 hidden: !state.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItem : addItemToCart(state.cartItem,action.payload)
             }
         default:
             return state;
