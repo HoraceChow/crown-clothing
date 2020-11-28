82c167d299f2be7a3ba3e08af9428e9c4e0f9ae6
Horace <horace1028@gmail.com>
2020-11-29 03:58:03 +0800

    created items added to cart component and connected cart dropdown to redux

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropDown = ()=>(
+const CartDropDown = ({cartItem})=>(
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+        {
+            cartItem.map(item => <CartItem key={item.id} item={item}/>)
+        }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
-export default CartDropDown;
+
+const mapStatetoProps = ({cart:{cartItem}}) => ({cartItem})
+export default connect(mapStatetoProps)(CartDropDown);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,8 +2,8 @@ export const addItemToCart = (cartItems,cartItemToAdd)=>{
     const exitingCartItem = cartItems.find(cartItem => cartItem.id===cartItemToAdd.id);
     if(exitingCartItem){
         return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id ? {...cartItem,quntity : cartItem.quntity+1} : cartItem
+            cartItem.id === cartItemToAdd.id ? {...cartItem,quantity : cartItem.quantity+1} : cartItem
         )
     }
-    return [...cartItems,{...cartItemToAdd,quntity:1}];
+    return [...cartItems,{...cartItemToAdd,quantity:1}];
 }
